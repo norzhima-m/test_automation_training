0fd42004415058fba5e7c5873894addf3192f9f5
Norzhima Morgunova <chagdurova1992@gmail.com>
2022-05-02 21:46:52 +0400

    Added base_page.py and main_page.py

---
+++ pages/main_page.py
@@ -0,0 +1,9 @@
+from webbrowser import BaseBrowser
+
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+    def open(self):
+        self.browser.get(self.url)
