3857028bbcdaf0ca1dfef521162ad494e79a0fbf
Norzhima Morgunova <chagdurova1992@gmail.com>
2022-05-03 20:37:23 +0400

    Added test_should_be_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from time import sleep
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -14,3 +15,11 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_sould_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+    sleep(5)
+
