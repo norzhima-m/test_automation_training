34859c171c5dc436a09964027723efa856559893
Norzhima Morgunova <chagdurova1992@gmail.com>
2022-05-03 21:14:07 +0400

    transition between pages

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    # sleep(5)
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -21,5 +22,4 @@ def test_sould_be_login_page(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
-    sleep(5)
 
