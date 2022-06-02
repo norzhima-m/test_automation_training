d2ee61f7b6af01f5f5a571b5c558bfb6186f15d7
Norzhima Morgunova <chagdurova1992@gmail.com>
2022-06-02 13:13:48 +0400

    test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 from time import sleep
 import pytest
-from .pages.login_page import LoginPage
 
 
 # @pytest.mark.skip
@@ -63,4 +64,14 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):    
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_products_in_basket()
+    basket_page.should_be_message_about_empty_basket()
 
